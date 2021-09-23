e7779e1e53447d774b9999ac84c92cfa01f09266
Kerr <sunjian2@shein.com>
2021-09-23 18:08:55 +0800

    2021年09月23日18:08:05：yygh设置模块继续1

---
+++ service/service_hosp/src/main/java/com/atguigu/yygh/ServiceHospApplication.java
@@ -0,0 +1,17 @@
+package com.atguigu.yygh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @FileName: ServiceHospApplication
+ * @Author: SunJian
+ * @Date: 2021/9/23 下午5:30
+ * @Description: 医院模块启动类
+ */
+@SpringBootApplication
+public class ServiceHospApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceHospApplication.class, args);
+    }
+}
