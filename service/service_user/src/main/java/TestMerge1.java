e8701b8c599c16b690423b739b6419595e4ca138
Kerr <sunjian2@shein.com>
2021-09-26 12:19:10 +0800

    分支2 java里的最新更新

--- service/service_user/src/main/java/TestMerge1.java
+++ service/service_user/src/main/java/TestMerge1.java
@@ -4,8 +4,8 @@
  * @Date: 2021/9/24 上午11:17
  * @Description: TODO //模块目的、功能描述
  */
-public class TestMerge {
+public class TestMerge1 {
     public static void main(String[] args) {
-        System.out.println("test");
+        System.out.println("test1");
     }
 }
