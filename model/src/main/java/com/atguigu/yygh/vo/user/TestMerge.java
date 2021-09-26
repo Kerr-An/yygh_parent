035305da3983ce5062b1e12a9435b96dc335f2d7
Kerr <sunjian2@shein.com>
2021-09-26 10:20:43 +0800

    我是分支hosp_dev_1的最新更新

--- model/src/main/java/com/atguigu/yygh/vo/user/TestMerge.java
+++ model/src/main/java/com/atguigu/yygh/vo/user/TestMerge.java
@@ -4,7 +4,10 @@ package com.atguigu.yygh.vo.user;
  * @FileName: testMerge
  * @Author: SunJian
  * @Date: 2021/9/24 上午11:17
- * @Description: TODO //模块目的、功能描述
+ * @Description: testMerge测试从不同分支合并，解决冲突
  */
 public class TestMerge {
+    static {
+        String str1 = "我是分支hosp_dev_1的最新更新";
+    }
 }
